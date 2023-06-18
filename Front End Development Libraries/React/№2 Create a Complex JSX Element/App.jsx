8d52e06eae3b71244f31f633a97543ab198a984d
Kabakova <kabdia@mail.ru>
2023-06-18 22:35:23 +0500

    added folder 'React', tasks №1- 11

---
+++ Front End Development Libraries/React/№2 Create a Complex JSX Element/App.jsx
@@ -0,0 +1,9 @@
+const JSX = <div>
+<h1>Title</h1>
+<p>Абзац</p>
+   <ul>
+     <li>1</li>
+     <li>2</li>
+     <li>3</li>
+   </ul>
+</div>
---
+++ Front End Development Libraries/React/№7 Create a Stateless Functional Component/App.jsx
@@ -0,0 +1,11 @@
+const MyComponent = function() {
+    // Change code below this line
+  
+  
+  
+    // Change code above this line
+  return(
+    <div>
+    Hello
+    </div>
+  )}
